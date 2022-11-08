08b4027ac474bb37b9e2d8863065765a4605cfa7
bego <begomontes7@gmail.com>
2022-11-08 21:21:31 +0100

    Changes barcelona version2

--- scripts2.js
+++ scripts2.js
@@ -1,17 +1,16 @@
+$(document).ready(function(){
+  $("#primerModal").click(function(){
+    $('#exampleModal').hide();
+      
+    });
+});
 
-  $(document).ready(function(){
-    $("#primerModal").click(function(){
-      $('#exampleModal').hide();
-        
-      });
-  });
 
-  
-  $(document).ready(function(){
-    $(".close").click(function(){
-      $('#exampleModal').hide();
-      $('#ejemplos').hide(); 
-      $('#RSVP').hide(); 
-      $('#miModalito').hide();
-      });
-  });
+$(document).ready(function(){
+  $(".close").click(function(){
+    $('#exampleModal').hide();
+    $('#ejemplos').hide(); 
+    $('#RSVP').hide(); 
+    $('#miModalito').hide();
+    });
+});
